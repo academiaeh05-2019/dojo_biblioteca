2a94cf2b02deb14cd41002749cbd7901b5e6d5d9
Gabriel Roger <gabriel.nroger@gmail.com>
2019-02-28 17:43:07 -0300

    Adicionar paginas e methods funcionais

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -3,19 +3,14 @@ from django.db import models
 class Livro(models.Model):
     titulo = models.CharField(max_length=200)
     lancamento = models.DateField()
-    descricao = models.CharField(max_length=2000)
+    descricao = models.TextField()
+    autor = models.CharField(max_length=256, default='')
+    genero = models.CharField(max_length=256, default='')
 
-class Autor(models.Model):
-    nome = models.CharField(max_length=200)
-    nascimento = models.DateField()
-    nacionalidade = models.CharField(max_length=200)
-    livro = models.ForeignKey(Livro, on_delete=models.SET_DEFAULT, default='')
+    def __str__(self):
+        return self.titulo
 
 class Editora(models.Model):
     nome = models.CharField(max_length=200)
     pais = models.CharField(max_length=200)
-    livro = models.ForeignKey(Livro, on_delete=models.SET_DEFAULT, default='')
-
-class Genero(models.Model):
-    nome = models.CharField(max_length=50)
     livro = models.ForeignKey(Livro, on_delete=models.SET_DEFAULT, default='')
--- dojo_biblioteca/urls.py
+++ dojo_biblioteca/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from biblioteca import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.render_index),
+    path('cadastrar/livros', views.render_cadastrar_livros)
 ]
