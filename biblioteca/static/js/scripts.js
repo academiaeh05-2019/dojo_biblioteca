219cac91da725e116531975c1761c40855436091
Felipe Teixeira Moraes <fmoraes@mastertech.tech>
2019-02-28 21:09:27 -0300

    Adicionar menu mobile, corrigir nav para que ela fique abaixo do header e por cima do conteúdo da página

--- biblioteca/static/js/scripts.js
+++ biblioteca/static/js/scripts.js
@@ -1,1 +1,7 @@
-// alert('bem-vindo')
+const botaoMenuMobile = document.querySelector('.menu-mobile');
+const navbar = document.querySelector('nav');
+
+botaoMenuMobile.onclick = function () {
+    botaoMenuMobile.classList.toggle('ativo');
+    navbar.classList.toggle('ativo');
+};
